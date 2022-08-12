3cdc2203f0581b20f2a225bfed11cde4a88b7fd5
Shashwot Pradhan <shashwotpradhan@gmail.com>
2022-08-12 14:46:24 +0545

    adding new input field

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,32 @@ const App = () => {
 
     const [name, setName ] = useState();
     const [fullName, setFullName ] = useState();
+    const [lastName, setLastName] = useState();
+    const [lastNamenew, setLastNameNew] = useState();
+
     const inputEvent = (event) => {
         console.log(event.target.value);
          setName(event.target.value);
     }
-    const onsubmit = () => {
+    const inputEventTwo = (event) => {
+        setLastName(event.target.value);
+    }
+    const onsubmit = (event) => {
+        event.preventDefault();
         setFullName(name);
+        setLastNameNew(lastName);
     }
     return (
  <>
+<form onSubmit={onsubmit}> 
     <div>
-        <h1>Hello {fullName} </h1>
+        <h1>Hello {fullName} {lastNamenew} </h1>
         <input type='text' placeholder="Enter your name" onChange={inputEvent} value={name} />
-        <button onClick={onsubmit}>Click Me</button> 
+        <br />
+        <input type='text' placeholder="Enter your password" onChange={inputEventTwo} value={lastName} />
+        <button type="submit"  >Submit Me</button> 
     </div>
+</form>
  </>
     );
 }
